386e63a15dccfc5e73d64db565c8fbee37afb342
tjrwls1211 <seokjin6635@gmail.com>
2025-10-18 01:16:31 +0900

    test: 쉼표 및 콜론 구분자 처리 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -17,6 +17,14 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 쉼표_클론_입력() {
+        assertSimpleTest(() -> {
+            run("1,2:3");
+            assertThat(output()).contains("결과 : 6");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
