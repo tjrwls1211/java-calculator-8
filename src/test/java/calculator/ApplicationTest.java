b24cd2ecf49c2b908ef3cbacdf6242398d0b9601
tjrwls1211 <seokjin6635@gmail.com>
2025-10-18 00:08:00 +0900

    test: 빈 문자열 입력 시 0 반환 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -9,6 +9,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 빈문자열_입력시_0을_출력() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
